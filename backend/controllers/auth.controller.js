5cc6ace4951d40c5761649119c1168c87e2cfe67
Nitin Kumar <nk9465110@gmail.com>
2026-07-06 16:22:15 +0530

    Project setup, DB conn and User model

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,5 @@
+const signupUser = async () => {
+    
+}
+
+export { signupUser }
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import 'dotenv/config'
+import { connectDB } from './lib/db.js'
+
+import authRoutes from './routes/auth.route.js'
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    connectDB()
+    console.log(`Server is running at PORT: ${PORT}`)
+})
